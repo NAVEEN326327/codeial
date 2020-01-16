529b5c4aa8a1d035f15c153d95891e40752a27e1
NAVEEN326327 <nks326327@gmail.com>
2020-01-16 14:23:46 +0530

    EJS pages setup

--- controllers/signup_controller.js
+++ controllers/signup_controller.js
@@ -1,3 +1,8 @@
 module.exports.signup = (req,res)=>{
-     res.end("<h2>Signup page</h2> ");
+   //   res.end("<h2>Signup page</h2> ");
+   return res.render('signup',{
+      title: "Signup page"
+
+   }
+   )
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(err,res){
-    res.end('<h1>User Profile</h1>');
+    // res.end('<h1>User Profile</h1>');
+    return res.render('profile', {
+        title: "Profile Page"
+    });
 }
