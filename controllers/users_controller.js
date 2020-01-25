cdf1452da4ea03afe82e031f95dab8c7ef09487f
NAVEEN326327 <nks326327@gmail.com>
2020-01-25 23:01:19 +0530

    signin with manual authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -50,26 +50,26 @@ module.exports.createSession = (req, res) => {
 
     // steps to authenticate
 
-    // find the user
-    // User.findOne({ email: req.body.email }, function (err, user) {
-    //     if (err) { console.log('error in finding user in signing in'); return }
-    //     // handle user found
-    //     if (user) {
-    //         //handle password which doesn't match
-    //         if (user.password != req.body.password) {
-    //             return res.redirect('back');
-    //         }
-    //         //handle session creation
-    //         res.cookie('user_id', user.id);
-    //         return res.redirect('/users/profile');
+    //find the user
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (err) { console.log('error in finding user in signing in'); return }
+        // handle user found
+        if (user) {
+            //handle password which doesn't match
+            if (user.password != req.body.password) {
+                return res.redirect('back');
+            }
+            //handle session creation
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
 
-    //     } else {
-    //         // handle user not found
+        } else {
+            // handle user not found
 
-    //         return res.redirect('back');
-    //     }
+            return res.redirect('back');
+        }
 
-    // });
+    });
 
 
 
