0629170069865ecb0af8b9bea85bfe1c20e9777c
NAVEEN326327 <nks326327@gmail.com>
2020-02-21 16:26:21 +0530

    continue with manual authentication

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
-const port = 8001;
+const port = 8002;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+// used for session cookie
+const session = require('express-session');
+const passport = require('passport');
+// const passportLocal = require('./config/passport-local-strategy');
+
+const sassMiddleware = require('node-sass-middleware');
 
 
+
+app.use(sassMiddleware({
+    src: '/assests/scss', 
+    dest: '/assets/css',
+    debug: true,
+    
+}))
 app.use(express.urlencoded());
 
 app.use(cookieParser());
@@ -19,12 +32,28 @@ app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
 
-//use express router
-app.use('/', require('./routes'));
+
 
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployement in production mode
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100) // time in mili second
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//use express router
+app.use('/', require('./routes'));
 
 app.listen(port, (err)=>{
     if(err){
