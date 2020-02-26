0c3db6a2f3627e06d2e751a448810b07c083b1e8
NAVEEN326327 <nks326327@gmail.com>
2020-02-26 14:59:01 +0530

    authentication checking using passport part-2

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -8,7 +8,7 @@ const User = require('../models/user');
 passport.use(new LocalStrategy({
     usernameField: 'email'
 }, 
- (email,password, done)=>{
+ (email,password, done)=>{  // done is a callback funtion
      // find a user and establish the identity
      User.findOne({email: email}, (err,user)=>{
          if(err){
@@ -42,5 +42,23 @@ return done(null, user);
     });
 });
 
+// check if the user is authenticated
+passport.checkAuthentication = (req, res, next)=>{
+    // if the user is signed in, then pass on the request to the next function(controller's action)
+    if (req.isAuthenticated()){
+        return next();
+    }
+    //  if the user is not signed in
+    return res.redirect('/users/sign-in');
+}
+
+passport.setAuthenticatedUser = (req, res, next)=>{
+    if(req.isAuthenticated()){
+        // req.user contains the current signed in user from the session and we are just sending this to the locals for views
+        res.locals.user = req.user;
+    }
+
+    next();
+}
 
 module.exports = passport;
