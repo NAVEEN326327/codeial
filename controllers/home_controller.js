a424dcd4481f2ba783e2e88f736f1a8448be84cd
NAVEEN326327 <nks326327@gmail.com>
2020-03-11 13:03:37 +0530

    populate all posts on home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,27 @@
+const Post = require('../models/post');
+
 module.exports.home = (req,res)=>{
    // return res.end("<h1>Express is up for Codeial!</h1>");
 //   console.log(req.cookies);
-//   res.cookie('name', "kumar sharma");
-  
-     return res.render('home', {
-        title: "Home"
-    });
+// //   res.cookie('name', "kumar sharma");
+//   Post.find({}, (err,posts)=>{
+//     return res.render('home', {
+//         title: "Codeial | Home",
+//         posts: posts
+        
+//     });
+//   })  
+
+
+// populate the user of each post
+  Post.find({}).populate('user').exec((err,posts)=>{
+      return res.render('home', {
+          title: "Codeial | Home",
+          posts: posts
+
+      });
+  });
+
 }
 
 // module.exports.actionName = function(req,res){}
