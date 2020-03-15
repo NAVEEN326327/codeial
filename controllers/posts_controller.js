6e563dcd521b0ed8cc7a9024b48772cf724f15f9
NAVEEN326327 <nks326327@gmail.com>
2020-03-15 23:21:48 +0530

    Deleting a post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,13 +1,32 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
-module.exports.create = (req,res)=>{
+module.exports.create = (req, res) => {
     Post.create({
         content: req.body.content,
         user: req.user._id
-    }, (err,post)=>{
-       if(err){console.log('error in creating a post'); return;}
+    }, (err, post) => {
+        if (err) { console.log('error in creating a post'); return; }
 
-       return res.redirect('back');
+        return res.redirect('back');
 
-    } );
+    });
+}
+
+
+module.exports.destroy = (req, res) => {
+    Post.findById(req.params.id, (err, post) => {
+
+        // .id means converting the object id into string
+        if (post.user == req.user.id) {
+            post.remove();
+
+            Comment.deleteMany({ post: req.params.id }, (err) => {
+                return res.redirect('back');
+            });
+        }else{
+            return res.redirect('back');
+        }
+
+    });
 }
