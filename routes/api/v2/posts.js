f8900790a186baa53044f5e271d9e36eb7128579
NAVEEN326327 <nks326327@gmail.com>
2020-03-28 10:55:33 +0530

    API delete request

--- routes/api/v2/posts.js
+++ routes/api/v2/posts.js
@@ -2,9 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-const postsApi = require('../../../controllers/api/v1/posts_api');
+const postsApi = require('../../../controllers/api/v2/posts_api');
 
 router.get('/', postsApi.index);
-router.delete('/:id', postsApi.destroy);
 
 module.exports = router;
