0aeda7e967b76a16ff036a772e2c19501bd016f3
NAVEEN326327 <nks326327@gmail.com>
2020-04-04 15:06:06 +0530

    chat application part created using socket.io

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,3 +1,24 @@
 module.exports.chatSockets = (socketServer)=>{
-    
+    let io = require('socket.io')(socketServer);
+
+    io.sockets.on('connection', (socket)=>{
+        console.log('new connection received', socket.id);
+
+        socket.on('disconnect', ()=>{
+            console.log('socket disconnected');
+        });
+        socket.on('join_room', (data)=>{
+            console.log('joining request rec.', data);
+
+            socket.join(data.chatroom);
+
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+
+        // Change :: detect send_message and broadcast to everyone in the room
+        socket.on('send_message', (data)=>{
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+
+    });
 }
---
+++ models/chat.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const chatSchema = new mongoose.Schema({
+
+    
+
+
+});
+
+
+const Chat = mongoose.model('Chat', ChatSchema);
+module.exports = Chat;
